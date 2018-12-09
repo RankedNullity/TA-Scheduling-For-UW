087929a3aa775dee3599cc455da611713f3b9317
TheNightly <jierui1998@gmail.com>
2018-12-09 01:18:54 -0800

    Working Output generation. Working on displaying output.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 (function() {
   "use strict";
   /*************************** Variables ******************************/
-
   /**
    * The starting point in our program, setting up a listener
    * for the "load" event on the window. When this event occurs,
@@ -11,8 +10,99 @@
 
 
   function initialize() {
+    $('Generate-Button').addEventListener('click', function() {
+      let input = document.querySelectorAll('input');
+      let totalTAs = input[input.length - 1].value * input[input.length - 2].value;
+      console.log("total TAs: " + totalTAs);
+      let students = [];
+      for (let i = 0; i < input.length - 2; i++) {
+        students.push(input[i].value);
+      }
+      console.log("Student Populations: " + students);
+      let distribution = jefferson(students, totalTAs);
+      console.log("distribution: " + distribution)
+      generateOutput(distribution);
+    });
   }
 
+  function generateOutput(distribution) {
+    let table = $("Answer-Table");
+    table.appendChild($("Input-Table").cloneNode(true));
+    let answer = document.querySelectorAll('#Input-Table')[1];
+    console.log(answer);
+  }
 
-  // Add the navBarHeightclass to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  /**
+   * Uses Jefferson's Method to apportion the TAs.
+   * @param {int} tas - Number of total TA hours.
+   * @param {Array} studentDistr - The raw student values as an array.
+   * @returns {Array} with the number of integer TAs in the corresponding slots.
+   */
+  function jefferson(studentDistr, tas) {
+    let totalStudents = sum(studentDistr);
+    let stdDivisor = totalStudents / tas;
+    console.log("Standard Divisor: " + stdDivisor);
+    let exactQuotas = getQuotas(studentDistr, stdDivisor);
+    console.log(exactQuotas);
+    let result = arrayFloor(exactQuotas);
+    console.log(result);
+    let total = sum(result);
+    console.log(total);
+    let stepSize = totalStudents / 10000;
+    while (total < tas) {
+      stdDivisor -= stepSize;
+      exactQuotas = getQuotas(studentDistr, stdDivisor);
+      result = arrayFloor(exactQuotas);
+      total = sum(result);
+      console.log("result: " + result);
+      console.log("total: " + total);
+    }
+    return result;
+  }
+
+  /**
+   * Takes a student distribution and std divisor and returns the Array
+   * with the quotas calculated using those values.
+   */
+  function getQuotas(studentDistr, stdDivisor) {
+    let result = [];
+    for (let i = 0; i < studentDistr.length; i++) {
+      result.push(studentDistr[i] === 0 ? null : studentDistr[i] / stdDivisor);
+    }
+    return result;
+  }
+
+  /**
+   * Floors each value in array. If value is null, the output value at the
+   * corresponding index are null.
+   */
+  function arrayFloor(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+      result.push(array[i] === null ? null : Math.floor(array[i]));
+    }
+    return result;
+  }
+
+  /**
+   * Returns the sum over array.
+   * @param {int[]} array
+   * @returns {int} sum over the array.
+   */
+  function sum(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += Number(array[i]);
+    }
+    return total;
+  }
+
+  /**
+   * Helper function for shortening getElementById.
+   * @param {String} id - Id.
+   */
+  function $(id) {
+    return document.getElementById(id);
+  }
+
 })();
