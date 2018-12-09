76c388c44be6797a25bc4bdaf7d993b239674258
TheNightly <jierui1998@gmail.com>
2018-12-08 23:30:24 -0800

    Developed Webapp structure for tool.

---
+++ app.js
@@ -0,0 +1,18 @@
+(function() {
+  "use strict";
+  /*************************** Variables ******************************/
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window. When this event occurs,
+   * the attached function (initialize) will be called.
+   */
+  window.addEventListener("load", initialize);
+
+
+  function initialize() {
+  }
+
+
+  // Add the navBarHeightclass to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+})();
